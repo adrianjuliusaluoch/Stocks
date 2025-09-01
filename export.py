b681cfbc518d048537c7623bd9e538b5fc4a16e9
Adrian Julius Aluoch <adrianjuliusaluoch@gmail.com>
2025-09-01 06:47:26 +0300

    Update export.py

--- export.py
+++ export.py
@@ -34,13 +34,13 @@ worksheet = spreadsheet.sheet1  # Select the first sheet
 all_records = worksheet.get_all_records()
 num_rows = len(all_records)
 
-# Check if there are more than 30 rows of data
-if num_rows <= 31:
+# Check if there are more than 120 rows of data
+if num_rows <= 121:
     print(f"Only {num_rows} rows found. Exiting without processing.")
-    sys.exit()  # Exit the script if 30 or fewer rows are found
+    sys.exit()  # Exit the script if 120 or fewer rows are found
 
 # Extract Data, Convert to DataFrame
-df = pd.DataFrame(worksheet.get('A2:Z31'), columns=worksheet.row_values(1))
+df = pd.DataFrame(worksheet.get('A2:Z121'), columns=worksheet.row_values(1))
 
 # Original Data
 data = df.copy().clean_names()
@@ -59,7 +59,8 @@ while job.state != 'DONE':
     print(job.state)
 
 # Delete Exported Rows
-worksheet.delete_rows(2, 31)
+worksheet.delete_rows(2, 121)
 
 # Exit 
 print(f'Cryptocurrency Data Export to Google BigQuery Successful')
+
